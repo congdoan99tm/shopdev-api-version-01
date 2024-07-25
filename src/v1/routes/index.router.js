19997762cc19854ae80d0c0c5fdfa829898c827b
Congdoan99tm <congdoan@dinos.vn>
2024-07-25 15:29:12 +0700

    FEAT: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -2,10 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    message: 'api ok',
+  });
+});
+
+router.get('/api/user', (req, res, next) => {
+  res.status(200).json({
+    status: 'success api',
+    message: 'api ok',
+    metadata: [
+      {
+        name: 'doan',
+        age: 27,
+      },
+      {
+        name: 'nacuto',
+        age: 26,
+      },
+      {
+        name: 'Dương to Quá',
+        age: 250,
+      },
+    ],
+  });
+});
 
 module.exports = router;
