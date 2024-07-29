97628fe42b794c656019019384d979e01a677b9a
doanlc <doanlc@hocmai.vn>
2024-07-29 19:18:27 +0700

    fix api 404

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/api/check-status', (req, res, next) => {
   res.status(200).json({
